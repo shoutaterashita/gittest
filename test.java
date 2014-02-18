d7000e248324093caeb1f689d26ed3436391a622
shoutaterashita <shoutaterashita@gmail.com>
2014-02-18 11:47:05 +0900

    test.javaを作成しました。

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class test {
+	public static void main(String[] args) {
+			BufferReader inBuffer =
+				new BufferReader(new InputStreamReader (System.in));
+			String str = input.readLine();
+
+			System.out.println("入力された文章は" + str + "です。");
+	}
+}
